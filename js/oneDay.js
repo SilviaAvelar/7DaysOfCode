5338534a7963425a2e4c3215e1b887ed29188482
Silvia-Avelar <avelar.silvia@yahoo.com>
2025-02-28 20:13:16 +0000

    Adicionar forDay.js

--- js/oneDay.js
+++ js/oneDay.js
@@ -7,19 +7,22 @@ let stringTrinta = '30';
 let numeroDez = 10;
 let stringDez = '10';
 
-if (numeroUm == stringUm && numeroUm !== stringUm) {
+// Comparando numeroUm e stringUm
+if (numeroUm == stringUm) {
     console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes.');
 } else {
     console.log('As variáveis numeroUm e stringUm não têm o mesmo valor.');
 }
 
+// Comparando numeroTrinta e stringTrinta
 if (numeroTrinta === stringTrinta) {
     console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor e mesmo tipo.');
 } else {
     console.log('As variáveis numeroTrinta e stringTrinta têm o mesmo valor, mas tipos diferentes.');
 }
 
-if (numeroDez == stringDez && numeroDez !== stringDez) {
+// Comparando numeroDez e stringDez
+if (numeroDez == stringDez) {
     console.log('As variáveis numeroDez e stringDez têm o mesmo valor, mas tipos diferentes.');
 } else {
     console.log('As variáveis numeroDez e stringDez não têm o mesmo valor.');
